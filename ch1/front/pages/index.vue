feb0745f1975fa92d9d9806a067099749a86a987
gilsu <rlftn510@naver.com>
2019-11-23 21:58:56 +0900

    [기능추가] 인피티니 스크롤 구현

--- ch1/front/pages/index.vue
+++ ch1/front/pages/index.vue
@@ -26,9 +26,31 @@
       },
       mainPosts(){
         return this.$store.state.posts.mainPosts;
+      },
+      hasMorePost() {
+        return this.$store.state.posts.hasMorePost
       }
-    }
+    },
     // layout : "default"
+    
+    fetch({store}) {
+      store.dispatch('posts/loadPost')
+    },
+    mounted() {
+      window.addEventListener('scroll', this.onScroll)
+    },
+    beforeDestory(){
+      window.removeEventListener('scroll', this.onScroll)
+    },
+    methods: {
+      onScroll() {
+        if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+          if(this.hasMorePost) {
+            this.$store.dispatch('posts/loadPost')
+          }
+        }
+      }
+    },
   }
 </script>
 
--- ch1/front/store/posts.js
+++ ch1/front/store/posts.js
@@ -1,6 +1,10 @@
 export const state = () => ({
   mainPosts: [],
+  hasMorePost : true,
 });
+
+const totalPosts = 31
+const limit = 10
 
 export const mutations = {
   addMainPost(state, payload) {
@@ -13,6 +17,23 @@ export const mutations = {
   addComment(state, payload) {
     const index = state.mainPosts.findIndex(v => v.id === payload.postId);
     state.mainPosts[index].Comments.unshift(payload);
+  },
+  loadPosts(state) {
+    const diff = totalPosts - state.mainPosts.length;
+    const fakePosts = Array(diff > limit ? limit : diff).fill().map( v => (
+      {
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: 'Andrew',
+        },
+        content: `Hello infinity scrolling ${Math.random()}`,
+        Comments: [],
+        Images : [],
+      }
+    ));
+    state.mainPosts = state.mainPosts.concat(fakePosts);;
+    state.hasMorePost = fakePosts.length === limit
   }
 };
 
@@ -27,4 +48,9 @@ export const actions = {
   addComment({ commit }, payload) {
     commit('addComment', payload);
   },
+  loadPost({commit, state}, payload) {
+    if(state.hasMorePost) {
+      commit('loadPosts')
+    }
+  }
 };
